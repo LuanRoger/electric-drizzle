88d2f479fb45374ebc5f0d8ae183e67508607365
ROG <luan.roger.2003@gmail.com>
2024-07-21 09:13:07 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import HomePage from "./pages/HomePage";
+import BaseLayout from "./layouts/BaseLayout";
+import { syncThemeWithLocal } from "./helpers/theme_helpers";
+import { useTranslation } from "react-i18next";
+import "./localization/i18n";
+import { updateAppLanguage } from "./helpers/language_helpers";
+
+export default function App() {
+    const { i18n } = useTranslation();
+
+    useEffect(() => {
+        syncThemeWithLocal();
+        updateAppLanguage(i18n);
+    }, []);
+
+    return (
+        <BaseLayout>
+            <HomePage />
+        </BaseLayout>
+    );
+}
+
+const root = createRoot(document.getElementById("app")!);
+root.render(<App />);
---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import { app, BrowserWindow } from "electron";
+import registerListeners from "./helpers/ipc/listeners-register";
+import path from "path";
+
+const inDevelopment = process.env.NODE_ENV === "development";
+
+if (require("electron-squirrel-startup")) {
+    app.quit();
+}
+
+function createWindow() {
+    const preload = path.join(__dirname, "preload.js");
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            devTools: inDevelopment,
+            contextIsolation: true,
+            nodeIntegration: true,
+            nodeIntegrationInSubFrames: false,
+
+            preload: preload,
+        },
+        titleBarStyle: "hidden",
+    });
+    registerListeners(mainWindow);
+
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    } else {
+        mainWindow.loadFile(
+            path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+        );
+    }
+}
+
+app.whenReady().then(createWindow);
+
+//osX only
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+//osX only ends
